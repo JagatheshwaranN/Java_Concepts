e1366fba0554ccb7912b88cc1c7152eff36ba574
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-02-11 12:58:07 +0530

    Java9 Factory Method Updated

---
+++ src/main/java/java9/feature/factorymethods/_03_FactoryMethodForSet_Demo3.java
@@ -0,0 +1,20 @@
+package java9.feature.factorymethods;
+
+import java.util.Set;
+
+/**
+ * 
+ * Java Set interface provides a Set.of() static factory method which is used to
+ * create immutable set.
+ * 
+ * @author Jagatheshwaran N
+ *
+ */
+public class _03_FactoryMethodForSet_Demo3 {
+
+	public static void main(String[] args) {
+
+		Set<String> topics = Set.of("Inheritance", "Polymorphism", "Abstraction", "Data hiding");
+		topics.stream().forEach(System.out::println);
+	}
+}
--- src/main/java/java9/feature/factorymethods/_04_FactoryMethodForSet_Demo4.java
+++ src/main/java/java9/feature/factorymethods/_04_FactoryMethodForSet_Demo4.java
@@ -1,14 +1,14 @@
 package java9.feature.factorymethods;
 
-import java.util.List;
+import java.util.Set;
 
-public class _02_FactoryMethodForList_Demo2 {
+public class _04_FactoryMethodForSet_Demo4 {
 
 	public static void main(String[] args) {
 
-		// static <E> List<E> of(E... elements) - When we have more than 10 elements
+		// static <E> Set<E> of(E... elements) - When we have more than 10 elements
 		// then it will use VarArg method
-		List<String> topics = List.of("Jshell", "Try With Resource", "Private Methods", "Factory Methods",
+		Set<String> topics = Set.of("Jshell", "Try With Resource", "Private Methods", "Factory Methods",
 				"Stream API enhancements", "Java Platform Module System", "Diamond Operator", "Safe VarArg annotations",
 				"Process API enhancements", "HTTP/2 client", "G1 Garbage Collector", "JLink");
 		topics.stream().sorted().forEach(System.out::println);
