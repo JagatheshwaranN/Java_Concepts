9fc8e998a29785aaa4a7959a798cd1cc2a9bc249
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-02-12 11:39:53 +0530

    Java9 Steam API Enhancement - DropWhile

--- src/main/java/java9/feature/stream/enhancements/_01_StreamAPIEnhancement_takeWhile_Demo1.java
+++ src/main/java/java9/feature/stream/enhancements/_01_StreamAPIEnhancement_takeWhile_Demo1.java
@@ -3,6 +3,20 @@ package java9.feature.stream.enhancements;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 
+ * It is the default method present in the Stream interface.
+ * 
+ * default Stream takeWhile (Predicate p)
+ * 
+ * takeWhile () method check until the condition is true. i.e., takeWhile()
+ * takes elements from stream as long as predicate returns true. If predicate
+ * returns false, at that point onwards remaining elements won’t be processed,
+ * i.e., rest of the stream is discarded.
+ * 
+ * @author Jagatheshwaran N
+ *
+ */
 public class _01_StreamAPIEnhancement_takeWhile_Demo1 {
 
 	public static void main(String[] args) {
---
+++ src/main/java/java9/feature/stream/enhancements/_02_StreamAPIEnhancement_dropWhile_Demo2.java
@@ -0,0 +1,27 @@
+package java9.feature.stream.enhancements;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ * It is the default method present in the Stream interface.
+ * 
+ * default Stream dropWhile (Predicate p)
+ * 
+ * It is the opposite of the takeWhile () method. It drops the elements instead
+ * of taking them as long as predicate returns true. Once predicate returns
+ * false then rest of the Stream will be returned.
+ * 
+ * @author Jagatheshwaran N
+ *
+ */
+public class _02_StreamAPIEnhancement_dropWhile_Demo2 {
+
+	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(2, 4, 8, 1, 6, 9, 10);
+		List<Integer> numbersAfterDropWhile = numbers.stream().dropWhile(n -> n % 2 == 0).collect(Collectors.toList());
+		System.out.println(numbersAfterDropWhile);
+	}
+}
