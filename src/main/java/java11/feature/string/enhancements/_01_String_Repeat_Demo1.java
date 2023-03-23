76eb31658b61d52707df8097092b42434be7cfca
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-03-23 09:46:08 +0530

    Java11 String API Enhancement Scenarios Added

---
+++ src/main/java/java11/feature/string/enhancements/_01_String_Repeat_Demo1.java
@@ -0,0 +1,17 @@
+package java11.feature.string.enhancements;
+
+/**
+ * Repeats a string given number of times. Returns the concatenated string. The
+ * repeat method simply repeats the string that many numbers of times as
+ * mentioned in the method in the form of an int.
+ * 
+ * @author Jagatheshwaran N
+ */
+public class _01_String_Repeat_Demo1 {
+
+	public static void main(String[] args) {
+
+		var topic = "Java String Repeat ";
+		System.out.println(topic.repeat(3));
+	}
+}
---
+++ src/main/java/java11/feature/string/enhancements/_02_String_IsBlank_Demo1.java
@@ -0,0 +1,20 @@
+package java11.feature.string.enhancements;
+
+/**
+ * Checks if a string is empty or have white spaces only. It returns a boolean
+ * value. Empty Strings and Strings with white spaces are only treated as blank.
+ * 
+ * @author Jagatheshwaran N
+ */
+public class _02_String_IsBlank_Demo1 {
+
+	public static void main(String[] args) {
+
+		var whiteSpace = " ";
+		var blank = "";
+		var topic = "String";
+		System.out.println(whiteSpace.isBlank());
+		System.out.println(blank.isBlank());
+		System.out.println(topic.isBlank());
+	}
+}
