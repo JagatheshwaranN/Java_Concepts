ae1b3f34ad390661f16d44b20c0e9ea71e737415
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-04-12 09:08:36 +0530

    Java13 Text Block Feature Scenario Added

---
+++ src/main/java/java13/feature/text/block/_01_Text_Block_Demo1.java
@@ -0,0 +1,43 @@
+package java13.feature.text.block;
+
+/**
+ * To define a multiline string, we used to use escape sequences for line breaks
+ * and double quotes contained in the string. Java 13 introduces text blocks to
+ * handle multiline strings like JSON/XML/HTML etc. It is a preview feature.
+ * Text Block allows to write multiline strings easily without using \r\n.
+ * 
+ * @author Jagatheshwaran N
+ */
+public class _01_Text_Block_Demo1 {
+
+	public static void main(String[] args) {
+
+		var text_json = """
+				{
+				   "name" : "John",
+				   "email" : "john@gmail.com"
+				}
+
+				""";
+		System.out.println(text_json);
+
+		var text_html = """
+				<html>
+					<head>
+						<link href='/css/style.css' rel='stylesheet' />
+					</head>
+					<body>
+						<h1>Hello World</h1>
+					</body>
+				</html>
+				""";
+		System.out.println(text_html);
+
+		var text_sql = """
+				SELECT id, firstName, lastName FROM Employee
+				WHERE departmentId = "IT"
+				ORDER BY firstName
+				""";
+		System.out.println(text_sql);
+	}
+}
