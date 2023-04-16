692ee47a57ad55853026a8933857c12f4eefda4f
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-04-16 09:58:03 +0530

    Java14 Helpful NPE Scenario Updated

---
+++ src/main/java/java14/feature/helpful/nullpointerexception/_02_Helpful_NullPointerException_Demo2.java
@@ -0,0 +1,11 @@
+package java14.feature.helpful.nullpointerexception;
+
+public class _02_Helpful_NullPointerException_Demo2 {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+
+		String name = null;
+		System.out.println(name.length());
+	}
+}
