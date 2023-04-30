25b5f37d9e0bc89ac60f8fbfb0550e57938c2c4e
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-04-30 10:34:56 +0530

    Java16 Sealed Class Scenarios Updated

--- src/main/java/java16/feature/sealed/classes/enhancement/_03_Sealed_Class_Demo3.java
+++ src/main/java/java16/feature/sealed/classes/enhancement/_03_Sealed_Class_Demo3.java
@@ -11,19 +11,46 @@ public class _03_Sealed_Class_Demo3 {
 
 	public static void main(String[] args) {
 
+		FastProcess fp = new FastProcess();
+		SlowProcess sp = new SlowProcess();
+		fp.getFastProcess();
+		sp.getSlowProcess();
 	}
 }
 
-sealed class Car {
+sealed class Process permits SlowProcess, FastProcess {
+}
 
-	public static void display() {
+final class SlowProcess extends Process {
 
-//		final class BMW extends Car { // Compile Time Error
-//
-//		}
+	public void getSlowProcess() {
+		System.out.println("Slow Process");
 	}
 }
 
-final class Audi extends Car {
+non-sealed class FastProcess extends Process {
 
+	public void getFastProcess() {
+		System.out.println("Fast Process");
+	}
+}
+
+/*
+ * The instanceof expression in the below code tests a reference of type Process
+ * to see if the object referred to by that reference implements the Loggable
+ * interface. Now, in the typical situation, all types that can ever be
+ * assignment-compatible with a sealed class will be known to the compiler, and
+ * if none implements the interface in question, the compiler could reject the
+ * code outright.
+ * 
+ * However, in this case, the FastProcess class is marked non-sealed, and
+ * because of this, it’s possible for this class to have subclasses that are not
+ * known to the compiler at this time, and those subclasses might possibly
+ * implement the Loggable interface. Because of this possibility, the compiler
+ * accepts the code and compiles the test as written.
+ */
+interface Loggable {
+	static boolean isLoggable(Process p) {
+		return p instanceof Loggable;
+	}
 }
