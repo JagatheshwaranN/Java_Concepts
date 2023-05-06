d41ce86dcb4922e8d9e4f45b27c797c46e00e702
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-06 10:06:18 +0530

    Java17 Strictfp Obsolete Scenario Added

---
+++ src/main/java/java17/feature/stricfp/obsolete/_01_Stricfp_Obsolete_Demo1.java
@@ -0,0 +1,17 @@
+package java17.feature.stricfp.obsolete;
+
+/**
+ * Modern hardware can perform strict floating-point semantics without
+ * performance degradation. So, it was decided in JDK Enhancement Proposal 306
+ * to make it the default semantics again, starting with Java 17. The strictfp
+ * keyword is thus obsolete.
+ * 
+ * @author Jagatheshwaran N
+ *
+ */
+public strictfp class _01_Stricfp_Obsolete_Demo1 {
+
+	public static void main(String[] args) {
+
+	}
+}
