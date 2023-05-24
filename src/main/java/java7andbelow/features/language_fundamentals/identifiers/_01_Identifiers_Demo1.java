42adb85f7dcde9984b5ded3a523f3c25ce9b19dd
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-05-24 09:02:28 +0530

    Core Java Concepts - Reserved Words Added

--- src/main/java/java7andbelow/features/language_fundamentals/identifiers/_01_Identifiers_Demo1.java
+++ src/main/java/java7andbelow/features/language_fundamentals/identifiers/_01_Identifiers_Demo1.java
@@ -8,20 +8,21 @@ package java7andbelow.features.language_fundamentals.identifiers;
  */
 public class _01_Identifiers_Demo1 {
 
-/*  Rules for defining Java Identifiers
+/**  Rules for defining Java Identifiers
 =======================================
-1.	a to z
-2.	A to Z
-3.	0 to 9
-4.	$
-5.	_
-1.	If any other characters are used, then compile time error will be triggered.
-2.	Identifiers can’t start with digits (numbers).
-3.	Identifiers are case-sensitive.
-4.	Identifiers have no length limit. But, it's recommended to not use lengthy identifier.
-5.	Identifiers can’t be a reserve word.
-6.	All predefined Java Class names and Interface names can be used as Identifiers.
-    But it’s not recommended to use the same and not a good programming practice.*/
+    1.	a to z
+    2.	A to Z
+    3.	0 to 9
+    4.	$
+    5.	_
+    1.	If any other characters are used, then compile time error will be triggered.
+    2.	Identifiers can’t start with digits (numbers).
+    3.	Identifiers are case-sensitive.
+    4.	Identifiers have no length limit. But, it's recommended to not use lengthy identifier.
+    5.	Identifiers can’t be a reserve word.
+    6.	All predefined Java Class names and Interface names can be used as Identifiers.
+        But it’s not recommended to use the same and not a good programming practice.
+ */
 
     int number = 10;
     String value = "Java";
