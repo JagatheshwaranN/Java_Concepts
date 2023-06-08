3d51e16cb5396ac5f3fceae78cd2eb8ee16c87b5
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-08 09:20:52 +0530

    Core Java Concepts - Variables - Local Variable & Local Level Uninit Array

---
+++ src/main/java/java7andbelow/features/language_fundamentals/arrays/_11_Uninitialized_Arrays_Local_Level.java
@@ -0,0 +1,16 @@
+package java7andbelow.features.language_fundamentals.arrays;
+
+public class _11_Uninitialized_Arrays_Local_Level {
+
+    public static void main(String[] args) {
+
+        int[] i1;
+        int[] i2 = new int[1];
+
+        // System.out.println(i1); - variable i1 might not have been initialized
+        // System.out.println(i1[0]); - variable i1 might not have been initialized
+
+        System.out.println(i2); // [I@4dd8dc3
+        System.out.println(i2[0]); // 0
+    }
+}
