7e8392f14407da71d01b6b8ae45c2b7a5c16f3e7
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-06-29 09:45:59 +0530

    Java Concepts - Break Statement Added

---
+++ src/main/java/java7andbelow/features/flow_control/transfer_statements/_01_Break_Statement.java
@@ -0,0 +1,47 @@
+package java7andbelow.features.flow_control.transfer_statements;
+
+public class _01_Break_Statement {
+
+    public static void main(String[] args) {
+
+        /*
+            Transfer Statements
+            ===================
+            We have 2 types of transfer statements such as Break and Continue.
+        */
+
+        /*  Break
+            =====
+            We can use the break statement in the following places as below,
+            1.	Inside Switch -To stop fall through
+            2.	Inside loop - To break loop execution based on some condition
+            3.	Inside labelled block - To break labelled block execution based on some condition
+        */
+        int x = 1;
+        switch(x){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("Zero");
+        }
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(i);
+            if (i == 1){
+                break;
+            }
+        }
+
+        labelledBlock:{
+            System.out.println("Begin");
+            if (x == 1){
+                break labelledBlock;
+            }
+            System.out.println("End");
+        }
+    }
+}
