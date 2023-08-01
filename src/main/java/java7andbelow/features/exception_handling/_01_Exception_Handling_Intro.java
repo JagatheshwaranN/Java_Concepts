730e8cc7c6a778faf855e23925428e2e699fba39
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-01 09:46:26 +0530

    Java Concepts - Exception Handling concepts - Intro

---
+++ src/main/java/java7andbelow/features/exception_handling/_01_Exception_Handling_Intro.java
@@ -0,0 +1,44 @@
+package java7andbelow.features.exception_handling;
+
+public class _01_Exception_Handling_Intro {
+
+    public static void main(String[] args) {
+
+        /*
+            Exception
+            =========
+            An unexpected, unwanted event that disturbs the normal flow of the program
+            is called as Exception.
+
+            Example,
+            TypePunctured Exception
+            Sleeping Exception
+            FileNotFound Exception
+
+            Note:
+            =====
+            It is highly recommended to handle the exception and the main objective of the
+            exception handling is graceful termination of the program.
+        */
+
+        /*
+            Exception Handling
+            ==================
+            Exception handling does not mean repairing an exception. We have to provide the
+            alternate way to continue the rest of the program normally. This is the concept
+            of Exception Handling.
+
+            For Example, our programming requirement is to read data from remote file located
+            at London. In runtime, if London file is not available then our program should not
+            be terminated abnormally. We have to provide some local file to continue the rest
+            of the program normally. This way of defining the alternative is called as
+            Exception Handling.
+            try {
+            Read data from London file
+            }
+            Catch(FileNotFoundException ex) {
+            Use local file
+            }
+        */
+    }
+}
