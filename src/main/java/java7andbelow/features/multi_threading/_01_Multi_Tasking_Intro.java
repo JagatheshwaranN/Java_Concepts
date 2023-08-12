71f2a24902bb423c3763047cb481cd6175f3b2b4
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-12 13:00:24 +0530

    Java Concepts - Multi Threading - MTasking Intro

---
+++ src/main/java/java7andbelow/features/multi_threading/_01_Multi_Tasking_Intro.java
@@ -0,0 +1,46 @@
+package java7andbelow.features.multi_threading;
+
+public class _01_Multi_Tasking_Intro {
+
+    public static void main(String[] args) {
+
+        /*
+            Multi-Tasking
+            =============
+            Executing several tasks simultaneously is a concept of Multi-Tasking.
+            There are 2 types of Multi-Tasking,
+            1.	Process Based Multi-Tasking
+            2.	Thread Based Multi-Tasking
+
+            Process Based Multi-Tasking
+            ===========================
+            Executing several tasks simultaneously where each task is a separate
+            independent program (process) is called as Process Based Multi Tasking.
+
+            Example,
+            While typing a java program in an editor, we can listen to audio song
+            from the same system. At the same time, we can download a file from the
+            internet. All these tasks will be executed simultaneously and independent
+            of each and hence it is Process Based MT. It's best suitable at OS level.
+
+            Thread Based Multi-Tasking
+            ==========================
+            Executing several tasks simultaneously where each task is a separate independent
+            part of the same program is called as Thread Based MT. Each independent part is
+            called as Thread. It's best suitable at programmatic level.
+
+            Main Objective of Multi Tasking
+            ===============================
+            Whether it is process based or thread based, the main objective of the Multi Tasking
+            is to reduce the response time of the system and to improve the performance.
+
+            Application Areas of Multi Tasking
+            ==================================
+            The main important application areas of MT are,
+            1.	To develop Multi Media graphics
+            2.	To develop Animations
+            3.	To develop Video Games
+            4.	To develop Web Servers and Application Servers, etc.
+        */
+    }
+}
