25d0b50e018bd555903a3dbe24e2aaf24b61140a
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-08-14 09:24:27 +0530

    Java Concepts - Multi Threading - Thread Case Scenarios

--- src/main/java/java7andbelow/features/multi_threading/_03_Thread_CaseScenarios.java
+++ src/main/java/java7andbelow/features/multi_threading/_03_Thread_CaseScenarios.java
@@ -12,10 +12,10 @@ public class _03_Thread_CaseScenarios {
             which order the threads will be executed is decided by Thread scheduler.
             We can't expect the exact algorithm followed by Thread scheduler, and it's
             varied from JVM to JVM and hence we can't expect the threads execution order
-            and exact order too. Hence, whenever situation comes to Multi Tasking there
+            and exact order too. Hence, whenever situation comes to Multi Threading there
             is no guarantee for exact output. But we can provide several possible outputs.
 
-            The following are the various possible outputs of the above program.
+            The following are the various possible outputs of the below program.
 
             Main Thread From _03_Thread_CaseScenarios Class
             Child Thread From MyThread1 Class
@@ -27,8 +27,8 @@ public class _03_Thread_CaseScenarios {
             ================================================
             In case of t.start(), a new thread will be created which is responsible for the
             execution of run() method. But, in case of t.run() a new thread won't be created
-            and run () will be executed just like a normal method call by main thread. Hence,
-            in the above program if we replace t.start() with t.run() then the output is as
+            and run() will be executed just like a normal method call by main thread. Hence,
+            in the below program if we replace t.start() with t.run() then the output is as
             below and the total output is produced by Main thread.
 
             Child Thread From MyThread1 Class
@@ -58,29 +58,105 @@ public class _03_Thread_CaseScenarios {
             If we are not overriding run() method, then Thread class run() method will be
             executed which has empty implementation. Hence, we won't get any output.
 
+            Case 6: Overriding of start()
+            =============================
+            If we override the start() method, then our start() method will be executed just
+            like a normal method call and new thread won't be created.
+
+            Note: It is not recommended to override the start() method, otherwise won't go
+                  for multi-threading concept.
+
+            Case 7: super.start()
+            =====================
+
+            Case 8: Thread Life Cycle
+            =========================
+            ------------    ------------------    -----------    --------
+            |New / Born| -> |Ready / Runnable| -> | Running | -> | Dead |
+            ------------    ------------------    -----------    --------
+            MyThread t = new MyThread () => New Thread
+            t.start ();                  => Thread will get Ready / Runnable
+            After Thread Scheduler allocates process. Thread will be moved to Running.
+            After run () method completes.
+            Thread will be moved to Dead state.
+
+            Case 9: IllegalThreadStateException
+            ===================================
+            After starting a Thread, if we are trying to restart the same Thread then
+            we will get the runtime exception saying IllegalThreadStateException.
+
         */
-        MyThread1 myThread = new MyThread1();
-        myThread.start();
-        // myThread.run();
+        MyThreadCase123Demo myThread1 = new MyThreadCase123Demo();
+        myThread1.start();
         for( int i = 0; i < 5; i++){
             System.out.println("Main Thread From _03_Thread_CaseScenarios Class");
         }
-        MyThread2 myThread2 = new MyThread2();
-        myThread2.start();
 
+        MyThreadCase4Demo myThread2 = new MyThreadCase4Demo();
+        myThread2.start();
+        // myThread.run();
+
+        MyThreadCase5Demo myThread3 = new MyThreadCase5Demo();
+        myThread3.start();
+
+        MyThreadCase6Demo myThread4 = new MyThreadCase6Demo();
+        myThread4.start();
+
+        MyThreadCase7Demo myThread5 = new MyThreadCase7Demo();
+        myThread5.start();
+
+        MyThreadCase9Demo myThread6 = new MyThreadCase9Demo();
+        myThread6.start();
+        // myThread6.start(); - Exception in thread "main" java.lang.IllegalThreadStateException
     }
 }
-
-class MyThread1 extends Thread {
+class MyThreadCase123Demo extends Thread {
     public void run(){
         for( int i = 0; i < 5; i++){
-            System.out.println("Child Thread From MyThread1 Class");
+            System.out.println("Child Thread From MyThreadCase123Demo Class");
         }
     }
     public void run(int i){
-            System.out.println("Parameterized run method From MyThread1 Class");
+            System.out.println("Parameterized run method From MyThreadCase123Demo Class");
+    }
+}
+class MyThreadCase4Demo extends Thread {
+    public void run(){
+        for( int i = 0; i < 5; i++){
+            System.out.println("Child Thread From MyThreadCase4Demo Class");
+        }
+    }
+    public void run(int i){
+        System.out.println("Parameterized run method From MyThreadCase4Demo Class");
+    }
+}
+class MyThreadCase5Demo extends Thread {
+}
+
+class MyThreadCase6Demo extends Thread {
+    public void start(){
+        for( int i = 0; i < 5; i++){
+            System.out.println("Start Method Override From MyThreadCase6Demo Class");
+        }
+    }
+    public void run(){
+        for( int i = 0; i < 5; i++){
+            System.out.println("Child Thread From MyThreadCase6Demo Class");
+        }
     }
 }
 
-class MyThread2 extends Thread {
+class MyThreadCase7Demo extends Thread {
+    public void start(){
+        super.start();
+        System.out.println("Start Method Override From MyThreadCase7Demo Class");
+    }
+    public void run(){
+        System.out.println("Child Thread From MyThreadCase7Demo Class");
+    }
+}
+class MyThreadCase9Demo extends Thread {
+    public void run(){
+        System.out.println("Child Thread From MyThreadCase9Demo Class");
+    }
 }
