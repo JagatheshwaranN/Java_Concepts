015c7b6a94170be459afadb31c7648e4879e5d55
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-09-21 09:30:03 +0530

    Java Concepts - Lang Package toString() & hashCode()

--- src/main/java/java7andbelow/features/lang_package/_02_ObjectClass_Part2.java
+++ src/main/java/java7andbelow/features/lang_package/_02_ObjectClass_Part2.java
@@ -15,7 +15,64 @@ public class _02_ObjectClass_Part2 {
             SOP(s) => SOP(s.toString());
             If our class doesn't contain toString() method, then object class toString() method
             will be executed.
-
         */
+
+        /*
+            In the below example, object class toString() method got executed which is implemented
+            as follows,
+
+            public String toString() {
+                return getClass().getName()+”@”+Integer.toHexString(hashCode());
+            }
+            classname@hascode_in_hexadecimalform
+        */
+        Student1 student1 = new Student1(101, "Alex");
+        System.out.println(student1);
+
+        /*
+            Based on our requirement, we can override toString() method to provide our own string
+            representation. For example, whenever we are trying to print student2 reference, to print
+            the student2 details we have to override the toString() as follows,
+
+            public String toString(){
+                return "Student [rollno "+rollno+" and "+"name "+name+"]";
+            }
+        */
+        Student2 student2 = new Student2(102, "John");
+        System.out.println(student2);
+
+        /*
+            Note:
+            =====
+            In all wrapper classes, collection classes, String classes, StringBuffer classes and
+            StringBuilder classes toString () method is overridden for meaningful string representation.
+        */
+
+    }
+}
+
+class Student1 {
+
+    int rollno;
+    String name;
+
+    Student1(int rollno, String name){
+        this.rollno = rollno;
+        this.name = name;
+    }
+}
+
+class Student2 {
+
+    int rollno;
+    String name;
+
+    Student2(int rollno, String name){
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    public String toString(){
+        return "Student [rollno "+rollno+" and "+"name "+name+"]";
     }
 }
