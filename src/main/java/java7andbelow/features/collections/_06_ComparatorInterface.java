440d9d1b3c3682c4db5d271bc357add40eb3c52a
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-11-05 09:41:40 +0530

    Java Concepts - Comparator Interface

--- src/main/java/java7andbelow/features/collections/_06_ComparatorInterface.java
+++ src/main/java/java7andbelow/features/collections/_06_ComparatorInterface.java
@@ -8,105 +8,117 @@ public class _06_ComparatorInterface {
     public static void main(String[] args) {
 
         /*
-            Comparable vs Comparator
-            ========================
-            1.	For predefined comparable classes (Like String), default natural sorting order
-                is already available. If we are not satisfied with that default natural sorting
-                order, then we can define our own sorting by using comparator.
+            Comparator(Interface)
+            =====================
+            Comparator present in java.util package, and it defines 2 methods as below.
+                1.	Compare
+                2.	Equals
 
-            2.	For predefined non-comparable classes (Like StringBuffer), default natural storing
-                order is not yet available. We can define our own sorting by using comparator.
+            public int compare(Object obj1, Object obj2);
+            1.	Returns -ve if obj1 has to come before obj2
+            2.	Returns +ve if obj1 has to come after obj2
+            3.	Returns 0 if both obj1 and obj2 are equal
 
-            3.	For our own classes like Employee, the person writing the class is responsible to
-                define the default natural sorting order by implementing comparable interface.
-                The person who is using our class, if he is not satisfied with default natural
-                sorting order, then he can define his own sorting using comparator interface.
+            public boolean equals(Object obj);
+            Whenever we are implementing comparator interface, compulsory we should provide
+            implementation for compare() method only, and we are not required to provide
+            implementation for equals() method.Because, its already available to our class
+            from Object class through inheritance.
         */
-        EmployeeNaturalSortingDemo.demo();
-        EmployeeCustomizedSortingDemo.demo();
+        ComparatorDemo1.demo();
+
+        /*
+            Note:
+            =====
+            If we are not passing a comparator object in TreeSet h = new TreeSet(new MyComparator()) line,
+            then internally JVM will call compareTo() method which is meant for default natural sorting
+            order.In this case, the output is [0, 5, 10, 15, 20].
+        */
+
+        /*
+            Various possible implementations of compare() method
+            ====================================================
+
+            public int compare (Object obj1, Object obj2) {
+            Integer I1 = (Integer)obj1;
+            Integer I2 = (Integer)obj2;
+            return I1.compareTo(I2); [Ascending Order]
+            return -I1.compareTo(I2); [Descending Order]
+            return I2.compareTo(I1); [Descending Order]
+            return -I2.compareTo(I1); [Ascending Order]
+            }
+
+            public int compare (Object obj1, Object obj2) {
+            return +1; [Insertion order]
+            return -1; [Reverse of insertion order]
+            return 0; [Only first element will be inserted]
+            }
+        */
+        ComparatorDemo2.demo();
     }
 
 }
 
-class Employee implements Comparable<Object> {
+class ComparatorDemo1 {
 
-    int id;
-
-    String name;
-
-    Employee(int id, String name) {
-        this.id = id;
-        this.name = name;
+    public static void demo() {
+        TreeSet<Object> treeSet = new TreeSet<>(new OwnComparator1());
+        treeSet.add(10);
+        treeSet.add(0);
+        treeSet.add(15);
+        treeSet.add(5);
+        treeSet.add(20);
+        System.out.println(treeSet);
     }
+
+}
+
+class OwnComparator1 implements Comparator<Object> {
 
     @Override
-    public String toString() {
-        return "Employee[" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ']';
-    }
-
-    public int compareTo(Object obj) {
-        Employee employee = (Employee) obj;
-        int id1 = this.id;
-        int id2 = employee.id;
-        if(id1 < id2) {
-            return -1;
-        } else if (id1 > id2) {
+    public int compare(Object o1, Object o2) {
+        Integer i1 = (Integer)o1;
+        Integer i2 = (Integer)o2;
+        if(i1 < i2){
             return +1;
-        } else {
+        } else if (i1 > i2) {
+            return -1;
+        }else {
             return 0;
         }
     }
 
 }
 
-class EmployeeNaturalSortingDemo {
+class ComparatorDemo2 {
 
     public static void demo() {
-        Employee employee1 = new Employee(101, "John");
-        Employee employee2 = new Employee(103, "Eric");
-        Employee employee3 = new Employee(102, "Alex");
-        Employee employee4 = new Employee(104, "Jeni");
-
-        TreeSet<Employee> treeSet = new TreeSet<>();
-        treeSet.add(employee1);
-        treeSet.add(employee2);
-        treeSet.add(employee3);
-        treeSet.add(employee4);
+        TreeSet<Object> treeSet = new TreeSet<>(new OwnComparator2());
+        treeSet.add("A");
+        treeSet.add(new StringBuffer("ABC"));
+        treeSet.add("XX");
+        treeSet.add("ABCD");
+        treeSet.add("A");
         System.out.println(treeSet);
     }
 
 }
 
-class MyComparator implements Comparator<Object> {
+class OwnComparator2 implements Comparator<Object> {
 
     @Override
     public int compare(Object o1, Object o2) {
-        Employee employee1 = (Employee)o1;
-        Employee employee2 = (Employee)o2;
-        String s1 = employee1.name;
-        String s2 = employee2.name;
-        return s1.compareTo(s2);
-    }
-
-}
-
-class EmployeeCustomizedSortingDemo {
-
-    public static void demo() {
-        Employee employee1 = new Employee(101, "John");
-        Employee employee2 = new Employee(103, "Eric");
-        Employee employee3 = new Employee(102, "Alex");
-        Employee employee4 = new Employee(104, "Jeni");
-
-        TreeSet<Employee> treeSet = new TreeSet<>(new MyComparator());
-        treeSet.add(employee1);
-        treeSet.add(employee2);
-        treeSet.add(employee3);
-        treeSet.add(employee4);
-        System.out.println(treeSet);
+        String s1 = o1.toString();
+        String s2 = o2.toString();
+        int length1 = s1.length();
+        int length2 = s2.length();
+        if(length1 < length2){
+            return -1;
+        } else if (length1 > length2) {
+            return +1;
+        }else {
+            return 0;
+        }
     }
 
 }
