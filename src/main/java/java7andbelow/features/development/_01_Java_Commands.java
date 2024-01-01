e8d8a68e4d9a0ccba47cde7961e325b1177484b0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-01-01 11:03:49 +0530

    Java Concepts - Dev - Jar File and Commands. Jar, War and Ear Files, WebServer Vs AppServer & WebApp Vs EJBApp

--- src/main/java/java7andbelow/features/development/_01_Java_Commands.java
+++ src/main/java/java7andbelow/features/development/_01_Java_Commands.java
@@ -42,7 +42,7 @@ public class _01_Java_Commands {
             Classpath describes the location where required .class files are available.
             Java Compiler and JVM will use classpath to locate required .class file.
 
-            By default, JVM will always searches in current working directory for the
+            By default, JVM will always search in the current working directory for the
             required .class file. If we set the classpath explicitly, then JVM will
             search in our specified classpath location and JVM won't search in CWD.
 
@@ -50,23 +50,23 @@ public class _01_Java_Commands {
             =================================================
             1.	By using environment variable classpath
             ===========================================
-            This way of setting classpath is permanent and will be preserved when system
-            restarts. Whenever we are installing a permanent software in a system, then
-            this approach is recommended.
+            This way of setting classpath is permanent and will be preserved when the
+            system restarts. Whenever we are installing permanent software in a system,
+            then this approach is recommended.
 
             2.	At command prompt level
             ===========================
             Using set command, Set classpath=c:\exercises
 
             This way of setting classpath will be preserved only for the particular CMD
-            session. Once, CMD window closes automatically the classpath goes off.
+            session. Once, CMD window closes automatically, the classpath goes off.
 
             3.	At command level
             ====================
-            By using -cp option. Java -cp c:\excercises Test
+            By using -cp option. Java -cp c:\exercises Test
 
             This way of setting classpath will be preserved only for the particular command.
-            Once, command execution completes, automatically classpath will be lost.
+            Once command execution completes, automatically classpath will be lost.
 
             Note:
             =====
@@ -121,7 +121,7 @@ public class _01_Java_Commands {
             }
 
             C:\> javac AStudent.java
-            D:\>javac ITIndustry.java
+            D:\> javac ITIndustry.java
             CTE: Cannot find symbol, symbol: class AStudent, location: class ITIndustry
 
             D:\>javac -cp C: ITIndustry.java
@@ -137,8 +137,8 @@ public class _01_Java_Commands {
             Example
             =======
             package pack1.pack2;
-            public Class Kareena {
-                public void m1 () {
+            public Class Karena {
+                public void m1() {
                 SOP ("Hi Saif, I need to set the ringtone");
                 }
             }
@@ -146,7 +146,7 @@ public class _01_Java_Commands {
             import package pack1.pack2;
             package pack3.pack4;
             public Class Saif {
-                public void m2 () {
+                public void m2() {
                     Kareena k = new Kareena();
                     k.m1();
                     SOP ("Not possible now, I'm little busy");
@@ -155,7 +155,7 @@ public class _01_Java_Commands {
 
             import package pack3.pack4;
             public Class Alex {
-                public void m3 () {
+                public void m3() {
                     Saif k = new Saif();
                     k.m2();
                     SOP ("Hey Kareena, Can I help you");
@@ -183,13 +183,13 @@ public class _01_Java_Commands {
             Example
             =======
             public Class Blake {
-                public void m1 () {
+                public void m1() {
                     SOP ("C: Blake");
                 }
             }
 
             public Class Blake {
-                public void m1 () {
+                public void m1() {
                     SOP ("D: Blake");
                 }
             }
@@ -204,7 +204,7 @@ public class _01_Java_Commands {
             =====
             1.	If any location is created because of a package statement, that location
                 should be resolved by using the import statement and base package location
-                we have to update in classpath.
+                we have to update in the classpath.
 
             2.	Compiler will check only one level dependency, whereas JVM will check all
                 levels of dependency.
