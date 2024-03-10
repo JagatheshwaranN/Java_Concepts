a2d052d6e61eacad555b7eef5c588864a59c7d61
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-10 21:37:37 +0530

    Java Concepts - MethodOverloading Notes Add In-progress.

---
+++ src/main/java/preparation/revise/mol/Demo9.java
@@ -0,0 +1,17 @@
+package preparation.revise.mol;
+
+public class Demo9 {
+
+    public static void main(String[] ar){
+        show(null);
+    }
+
+    public static void show(Object obj){
+        System.out.println("Object Display ==> "+obj);
+    }
+
+    public static void show(String str){
+        System.out.println("String Display ==> "+str);
+    }
+
+}
