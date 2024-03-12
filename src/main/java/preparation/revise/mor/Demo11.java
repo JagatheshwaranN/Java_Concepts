8b572acb7f6d15715a56a300b1d966344dce2259
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-12 21:38:20 +0530

    Java Concepts - MethodOverriding Notes Updated.

---
+++ src/main/java/preparation/revise/mor/Demo11.java
@@ -0,0 +1,26 @@
+package preparation.revise.mor;
+
+public class Demo11 {
+
+    public static void main(String[] args) {
+        DerivedClass<String> derivedClass = new DerivedClass<>();
+        String value = derivedClass.getValue();
+        System.out.println(value);
+    }
+
+    static class BaseClass<T> {
+        T getValue(){
+            System.out.println("BaseClass GetValue");
+            return null;
+        }
+    }
+
+    static class DerivedClass<T>  extends  BaseClass<T> {
+
+        @Override
+        T getValue(){
+            System.out.println("DerivedClass GetValue");
+            return null;
+        }
+    }
+}
---
+++ src/main/java/preparation/revise/mor/Demo15.java
@@ -0,0 +1,39 @@
+package preparation.revise.mor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class Demo15 {
+
+    public static  void main(String[] ar){
+        SubBase subBase = new SubBase();
+
+        Class<?> clazz = subBase.getClass();
+
+        try{
+            Method overriddenMethod = clazz.getMethod("overriddenMethod");
+            overriddenMethod.invoke(subBase);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static class Base {
+
+        public void baseMethod(){
+            System.out.println("Base Method");
+        }
+
+        public void overriddenMethod(){
+            System.out.println("Base - Overridden Method");
+        }
+    }
+
+    static class SubBase extends Base {
+        @Override
+        public void overriddenMethod() {
+            System.out.println("SubBase - Overridden Method");
+        }
+    }
+}
