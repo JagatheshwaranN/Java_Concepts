9e0b26b4fb0dec701893bad10f788d61717b8c96
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-13 21:52:22 +0530

    Java Concepts - Polymorphism Notes Updated.

---
+++ src/main/java/preparation/revise/Demo.java
@@ -0,0 +1,35 @@
+package preparation.revise;
+
+class Animal {
+    public void makeSound() {
+        System.out.println("Generic animal sound");
+    }
+
+    public static void eat() {
+        System.out.println("Generic animal eat");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+
+    public static void eat() {
+        System.out.println("Dog eat");
+    }
+
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        Animal animal2 = new Dog(); // Upcasting
+
+        animal1.makeSound(); // Output: Generic animal sound
+        animal2.makeSound(); // Output: Woof! (dynamic binding)
+        animal1.eat();
+        animal2.eat();
+    }
+}
