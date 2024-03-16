64472c6d217a5c5c747f8d3041c660523e0c0cc0
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-16 10:33:07 +0530

    Java Concepts - Inheritance Notes Add In-progress.

---
+++ src/main/java/preparation/revise/inherit/EnablePolymorphismDemo.java
@@ -0,0 +1,25 @@
+package preparation.revise.inherit;
+
+public class EnablePolymorphismDemo {
+
+    public static void main(String[] args) {
+        Animal animal = new Dog();
+        animal.makeSound();
+    }
+
+    public static class Animal {
+
+        public void makeSound(){
+            System.out.println("Animal makes sound");
+        }
+    }
+
+    public static class Dog extends Animal {
+
+        @Override
+        public void makeSound() {
+            System.out.println("Dog is barking");
+        }
+    }
+
+}
