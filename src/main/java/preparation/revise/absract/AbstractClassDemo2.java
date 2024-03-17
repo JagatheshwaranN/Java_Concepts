f87e6a77587c1b9264d4e455c0cb5d442de86c4c
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-17 10:20:08 +0530

    Java Concepts - Abstract Class Notes Add In-progress.

---
+++ src/main/java/preparation/revise/absract/AbstractClassDemo2.java
@@ -0,0 +1,44 @@
+package preparation.revise.absract;
+
+public class AbstractClassDemo2 {
+
+    public static void main(String[] args) {
+        SmartPhone smartPhone = new SmartPhone();
+        smartPhone.dailyUse();
+        smartPhone.compact();
+        smartPhone.gadget();
+        SmartPhone.variety();
+    }
+
+    public static abstract class Electronics {
+
+        public void gadget(){
+            System.out.println("Electronic Gadgets");
+        }
+
+        public abstract void compact();
+
+        public abstract void dailyUse();
+
+        public static void variety(){
+            System.out.println("Android and Iphone");
+        }
+
+    }
+
+    public static abstract class Mobile extends Electronics {
+
+        @Override
+        public void compact() {
+            System.out.println("Mobile device is compact");
+        }
+    }
+
+    public static class SmartPhone extends Mobile {
+
+        @Override
+        public void dailyUse() {
+            System.out.println("Smartphone is now daily use product");
+        }
+    }
+}
---
+++ src/main/java/preparation/revise/absract/AbstractClassDemo4.java
@@ -0,0 +1,20 @@
+package preparation.revise.absract;
+
+public class AbstractClassDemo4 {
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle() {
+            @Override
+            public void type() {
+                System.out.println("4 wheeler - SUV");
+            }
+        };
+        car.type();
+    }
+
+    public static abstract class Vehicle {
+        public abstract void type();
+
+    }
+
+}
