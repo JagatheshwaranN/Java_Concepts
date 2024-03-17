b55fd99708a0456ac37ec953d870eebc7ef4af53
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-17 13:45:21 +0530

    Java Concepts - Abstract Class Notes Add Completed.

---
+++ src/main/java/preparation/revise/absract/ACMethodSignatureDemo.java
@@ -0,0 +1,33 @@
+package preparation.revise.absract;
+
+public class ACMethodSignatureDemo {
+
+    public static void main(String[] args) {
+        ConcreteClass concreteClass = new ConcreteClass();
+        concreteClass.methodToHide();
+        ((SuperClass) concreteClass).methodToHide();
+        concreteClass.anotherMethod();
+    }
+
+    public static class SuperClass {
+        void methodToHide(){
+            System.out.println("Hide Method in SuperClass");
+        }
+    }
+    public abstract static class AbstractClass  extends SuperClass {
+        abstract void methodToHide();
+
+        void anotherMethod(){
+            System.out.println("Another method in AbstractClass");
+        }
+
+    }
+
+    public static class ConcreteClass extends AbstractClass {
+
+        @Override
+        void methodToHide() {
+            System.out.println("Hide Method in ConcreteClass");
+        }
+    }
+}
