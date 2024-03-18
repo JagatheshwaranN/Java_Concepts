ac03c6a9b7c59a66cc459fc8da215adbd73cdd81
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-18 21:15:02 +0530

    Java Concepts - Interface Notes Add Completed.

---
+++ src/main/java/preparation/revise/iface/CovariantIFaceDemo.java
@@ -0,0 +1,25 @@
+package preparation.revise.iface;
+
+public class CovariantIFaceDemo {
+
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        String value = myClass.display();
+        System.out.println(value);
+    }
+
+    public interface MyIFace1 {
+
+        Object display();
+    }
+
+    public static class MyClass implements MyIFace1 {
+
+        @Override
+        public String display() {
+            System.out.println("MyClass Display");
+            return "Covariant ReturnType";
+        }
+
+    }
+}
---
+++ src/main/java/preparation/revise/iface/PrivateIFaceDemo.java
@@ -0,0 +1,39 @@
+package preparation.revise.iface;
+
+public class PrivateIFaceDemo {
+
+    public static void main(String[] args) {
+        MyInterface myInterface = new MyClass();
+        myInterface.method();
+        MyInterface2 myInterface2 = new MyClass2();
+        myInterface2.method1();
+    }
+
+    private interface MyInterface {
+
+        void method();
+    }
+
+    public static class MyClass implements MyInterface {
+        @Override
+        public void method() {
+            System.out.println("MyClass Method");
+        }
+    }
+
+    protected interface MyInterface2 {
+
+        void method1();
+    }
+
+}
+
+
+class MyClass2 implements PrivateIFaceDemo.MyInterface2 {
+
+    @Override
+    public void method1() {
+        System.out.println("MyClass2 Method");
+    }
+
+}
