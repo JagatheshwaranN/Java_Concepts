dc4bcf7ef242b9236d9859e758d20a8c34eab085
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-21 10:53:18 +0530

    Java Concepts - Constructor Notes Add In-progress.

---
+++ src/main/java/preparation/revise/constructor/ConstructorChainingDemo.java
@@ -0,0 +1,39 @@
+package preparation.revise.constructor;
+
+public class ConstructorChainingDemo {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        Person person2 = new Person("John", 29);
+        Person person3 = new Person("Alex", 25, "London");
+        person1.display();
+        person2.display();
+        person3.display();
+    }
+
+    public static class Person {
+
+        String name;
+        int age;
+        String address;
+
+        public Person() {
+            this("Unknown", 0, "Unknown Address");
+        }
+
+        public Person(String name, int age){
+            this(name, age, "Unknown Address");
+        }
+
+        public Person(String name, int age, String address){
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public void display(){
+            System.out.println("Person Details - Name: " + name + ", Age: " + age + ", Address: " + address);
+        }
+    }
+}
---
+++ src/main/java/preparation/revise/constructor/ConstructorNonStaticMethodDemo.java
@@ -0,0 +1,36 @@
+package preparation.revise.constructor;
+
+public class ConstructorNonStaticMethodDemo {
+
+    public static void main(String[] args) {
+        new Derived();
+    }
+
+    public static class Base {
+
+        public Base() {
+            initialize();
+        }
+
+        public void initialize() {
+            System.out.println("Initialize from Base");
+        }
+    }
+
+    public static class Derived extends Base {
+
+        String message = "Initialize from Derived";
+
+        public Derived(){
+
+        }
+
+        @Override
+        public void initialize() {
+            System.out.println(message);
+        }
+
+    }
+}
+
+
