9e5f87789cd6fc2465b786ef9fca910f48f3472a
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-27 11:00:11 +0530

    Java Concepts - TransferStatement Notes Add Completed. SelectionStatement Notes Add Completed.

---
+++ src/main/java/preparation/revise/statements/LabelledSwitchDemo.java
@@ -0,0 +1,38 @@
+package preparation.revise.statements;
+
+public class LabelledSwitchDemo {
+
+    public static void main(String[] args) {
+
+        int i = 2;
+
+        outerSwitch:
+        switch(i) {
+
+            case 1:
+                System.out.println("Outer Switch - Case 1");
+                break;
+
+            case 2:
+                System.out.println("Outer Switch - Case 2");
+                switch (1){
+                    case 1:
+                        System.out.println("Inner Switch - Case 1");
+                        break outerSwitch;
+
+                    case 2:
+                        System.out.println("Outer Switch - Case 2");
+                        break;
+                }
+                break;
+
+            case 3:
+                System.out.println("Outer Switch - Case 3");
+                break;
+
+            default:
+                System.out.println("Outer Switch - default");
+        }
+        System.out.println("Out of outer switch");
+    }
+}
