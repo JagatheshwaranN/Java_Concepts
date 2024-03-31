62e6e5b59fe9fd00e17027116bef01f0339bd98d
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-03-31 15:07:54 +0530

    Java Concepts - Main Method Notes Add Completed.

---
+++ src/main/java/preparation/revise/main/MainMethodDemo4.java
@@ -0,0 +1,9 @@
+package preparation.revise.main;
+
+public class MainMethodDemo4 {
+
+    static public void main(String... args) {
+
+        System.out.println("Main Method with static public order of declaration");
+    }
+}
