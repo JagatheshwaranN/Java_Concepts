a474d1e6a8bc3e23ef3c92f81b6150bca754f005
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-06-05 20:36:34 +0530

    Java FP - Method Reference Usage

---
+++ src/main/java/preparation/fp_excercises/MethodReferenceUsage.java
@@ -0,0 +1,27 @@
+package preparation.fp_excercises;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class MethodReferenceUsage {
+
+    public static void main(String[] args) {
+
+        List<String> input = List.of("Spring", "Spring Boot", "API", "MicroServices", "AWS", "PCF", "Azure", "Docker", "Kubernetes");
+
+
+        input.stream()
+                // Member Reference with Non-Static Method
+                .map(String::toUpperCase)
+                // Member Reference with Static Method
+                .forEach(MethodReferenceUsage::display);
+
+        // Method Reference with Constructor
+        Supplier<String> data = String::new;
+        System.out.println(data.get());
+    }
+
+    private static void display(String value){
+        System.out.println(value);
+    }
+}
