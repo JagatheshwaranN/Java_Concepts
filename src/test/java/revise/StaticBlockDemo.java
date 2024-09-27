982ffd87147437ba8a346896301e4f8fa46e9aa7
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-09-27 19:12:15 +0530

    QuickNote - Static Block & Static Control Flow

---
+++ src/test/java/revise/StaticBlockDemo.java
@@ -0,0 +1,31 @@
+package revise;
+
+public class StaticBlockDemo {
+
+    final static int i = 20;
+
+    static int j = 10;
+
+    static int k;
+
+    static {
+        k = 5;
+        System.out.println("Static block - First statement to be executed");
+        demo();
+    }
+
+    static void demo() {
+        System.out.println("Static block demo");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+
+    static {
+        System.out.println("Last Static Block");
+    }
+
+}
---
+++ src/test/java/revise/StaticControlFlowDemo.java
@@ -0,0 +1,63 @@
+package revise;
+
+public class StaticControlFlowDemo {
+
+    static int i = 10;
+
+    static {
+        System.out.println("First static block");
+        System.out.println(i);
+        display();
+    }
+
+    public static void display() {
+        System.out.println("Static Method");
+        System.out.println(j);
+    }
+
+    static int j = 20;
+
+    public static void main(String[] args) {
+        display();
+        System.out.println("*******************************");
+        Low low = new Low();
+    }
+
+    static {
+        System.out.println("Second static block");
+        display();
+    }
+
+}
+
+class High {
+
+    static int i = 10;
+
+    static {
+        System.out.println(i);
+        System.out.println("High - Static block");
+        m1();
+    }
+
+    public static void m1() {
+        System.out.println("High - m1 method");
+    }
+
+}
+
+class Low extends High {
+
+    static int j = 20;
+
+    static {
+        System.out.println("Low - Static block");
+        m2();
+    }
+
+    public static void m2() {
+        System.out.println("Low - m1 method");
+        System.out.println(j);
+    }
+
+}
