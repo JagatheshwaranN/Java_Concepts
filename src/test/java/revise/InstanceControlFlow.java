7d299c18ca66ce5cd10b44f9b5a8867a90c30056
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-09-28 09:49:09 +0530

    QuickNote - Instance Block & Instance Control Flow. Combination of Static and Instance Control Flow.

---
+++ src/test/java/revise/InstanceControlFlow.java
@@ -0,0 +1,73 @@
+package revise;
+
+public class InstanceControlFlow {
+
+    int i = 10;
+    static int j = 20;
+
+    {
+        System.out.println("HighClass IIB block");
+        System.out.println(i);
+        System.out.println(j);
+        display();
+        show();
+    }
+
+    public void display() {
+        System.out.println("Instance Display Method");
+        System.out.println(k);
+    }
+
+    public static void show() {
+        System.out.println("Static Show Method");
+    }
+
+    int k = 30;
+    public static void main(String[] args) {
+        InstanceControlFlow demo = new InstanceControlFlow();
+        System.out.println("******************************");
+        LowClass lowClass = new LowClass();
+    }
+
+}
+
+class HighClass {
+    int i = 10;
+
+    {
+        System.out.println(i);
+        System.out.println("HighClass IIB block");
+        display();
+    }
+
+    public void display() {
+        System.out.println("HighClass Instance Display Method");
+    }
+
+    HighClass () {
+        System.out.println("HighClass Constructor");
+    }
+}
+
+class LowClass extends HighClass {
+
+    int j = 20;
+
+    {
+        System.out.println(j);
+        System.out.println("LowClass IIB block");
+        display2();
+    }
+
+    public void display2() {
+        System.out.println("LowClass Instance Display Method");
+        System.out.println(k);
+    }
+
+    int k = 30;
+    LowClass () {
+        System.out.println("LowClass Constructor");
+        System.out.println(k);
+    }
+
+}
