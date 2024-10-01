9e34a0fa325c4f558263c6573804e20e3d3c3151
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-10-01 08:43:14 +0530

    QuickNote - Transfer Statements

--- src/test/java/revise/SelectStatementDemo.java
+++ src/test/java/revise/SelectStatementDemo.java
@@ -6,6 +6,7 @@ public class SelectStatementDemo {
 
     public static void main(String[] args) {
         ifElseDemo();
+        switchDemo();
     }
 
     private static void ifElseDemo() {
@@ -23,6 +24,35 @@ public class SelectStatementDemo {
             }
         } else {
             System.out.println("Age is less than 18 years.");
+        }
+    }
+
+    private static void switchDemo() {
+        String day = "Wed";
+        switch (day) {
+            case "Mon":
+                System.out.println("Day 1");
+                break;
+            case "Tue":
+                System.out.println("Day 2");
+                break;
+            case "Wed":
+                System.out.println("Day 3");
+                break;
+            case "Thu":
+                System.out.println("Day 4");
+                break;
+            case "Fri":
+                System.out.println("Day 5");
+                break;
+            case "Sat":
+                System.out.println("Day 6");
+                break;
+            case "Sun":
+                System.out.println("Day 7");
+                break;
+            default:
+                System.out.println("Invalid Day");
         }
     }
 }
