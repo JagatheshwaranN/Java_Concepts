2ac36968f4ae366b86edf3f06f00298a50994b6b
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-10-04 09:12:43 +0530

    QuickNote - Threads / MultiThreading Add In-progress

--- src/main/java/beforeJava8/features/multi_threading/_07_Thread_Class_Constructors.java
+++ src/main/java/beforeJava8/features/multi_threading/_07_Thread_Class_Constructors.java
@@ -14,13 +14,13 @@ public class _07_Thread_Class_Constructors {
             Thread t5 = new Thread (ThreadGroup g, String name);
             Thread t6 = new Thread (ThreadGroup g, Runnable r);
             Thread t7 = new Thread (ThreadGroup g, Runnable r, String name);
-            Thread t8 = new Thread (ThreadGroup g, Runnable r, String name, long stacksize);
+            Thread t8 = new Thread (ThreadGroup g, Runnable r, String name, long stackSize);
         */
 
         // Another approach to Define a Thread (Not recommended to USE)
         MyThread1 thread = new MyThread1();
         Thread t = new Thread(thread);
-        t.start ();
+        t.start();
         System.out.println("Main Thread");
     }
 }
