446a798071c99473d65b75d18d6ea9f25307001c
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-07-06 17:57:56 -0500

    Java Revise Functional Programming

---
+++ src/test/java/com/v8/PredicateDemo.java
@@ -0,0 +1,29 @@
+package com.v8;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+
+    public static void main(String[] args) {
+
+        int[] input = {0, 5, 7, 10, 20, 30, 35, 40, 43, 47, 50};
+        Predicate<Integer> p1 = i-> i > 10;
+        Predicate<Integer> p2 = i-> i%2==0;
+        System.out.println("Numbers greater than 10:");
+        check(p1, input);
+        System.out.println("Even Numbers:");
+        check(p2, input);
+        System.out.println("Numbers NOT greater than 10:");
+        check(p1.negate(), input);
+        System.out.println("Numbers even and greater than 10:");
+        check(p1.and(p2), input);
+    }
+
+    static void check(Predicate<Integer> predicate, int[] arr) {
+        for(int val : arr) {
+            if(predicate.test(val)) {
+                System.out.println(val);
+            }
+        }
+    }
+}
